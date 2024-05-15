890afebafb2e2421e4046656f2cf32a46017b107
Tolga Bayır <80397811+tolgabayir@users.noreply.github.com>
2024-05-15 18:14:24 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,7 @@ export const environment = {
   MESSAGES: "messages",
   MESSAGE: "message",
   production: false,
-  firebase: {
-    apiKey: "AIzaSyA3LxQIHHH3k-Lo6VHaOHaIiC413LATcNA",
-    authDomain: "chatr-push-notification.firebaseapp.com",
-    projectId: "chatr-push-notification",
-    storageBucket: "chatr-push-notification.appspot.com",
-    messagingSenderId: "1043001049569",
-    appId: "1:1043001049569:web:37b84546b889e3e65052a0",
-    vapidKey: "BJhUxl5bSN6r3BIXBc8ik_BTXsbHS916qp0bUgvjMdP-c_NWUnlvRaCg6JVeDtQDhM0nWR8hf4OYqgn-T43yKoE"
-  }
+ 
 
 
 
